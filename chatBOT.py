e7743748ec44243ad040ec412ebe57712f53041e
lucifer950924 <amaiti.9509@gmail.com>
2026-01-11 13:42:04 +0530

    EnhancedKnowledgeBase_AddedRunningCode

--- chatBOT.py
+++ chatBOT.py
@@ -6,12 +6,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-def RAGChatbot(prompt: str):
-    ### Save the environment variables
-    deep_seek_key = decN("RAGChatBot_deepseek")
-
-    os.environ['OPENAI_API_KEY'] = deep_seek_key
-    os.environ['OPENAI_API_BASE'] = 'https://api.deepseek.com"'
+def splitTheKnowledgeBase():
     ### Declare the Current Working Directory
 
     currDir = os.getcwd()
@@ -31,21 +26,36 @@ def RAGChatbot(prompt: str):
     splitter = RecursiveCharacterTextSplitter(chunk_size = 800,chunk_overlap = 200)
     chunks = splitter.split_documents(documents)
 
+    return chunks
+
+def EmbedTheChunks(chunks:list):
     ### Embed and store the chunks in a Vector
 
 
     embeddings = HuggingFaceEmbeddings(model = "sentence-transformers/all-MiniLM-L6-v2")
     vector_store = FAISS.from_documents(chunks,embedding=embeddings)
     retriever = vector_store.as_retriever(search_kwargs={"k":3})
+    return retriever
+
+def RAGChatbot(prompt: str,retrieve):
+    ### Save the environment variables
+    deep_seek_key = decN("RAGChatBot_deepseek")
+
+    os.environ['OPENAI_API_KEY'] = deep_seek_key
+    os.environ['OPENAI_API_BASE'] = 'https://api.deepseek.com"'
+    
+
+    
     ##Creating LLM Model
     llm = ChatOllama(
         model="llama3:latest",
-        temperature = 0
+        temperature = 0.1,
+        verbose = True
         )
 
     ## Create Context for RAG
     query = prompt
-    docs = retriever.invoke(query)
+    docs = retrieve.invoke(query)
     context = "\n\n".join(doc.page_content for doc in docs)
     response = llm.invoke([
         HumanMessage(
@@ -63,15 +73,21 @@ def RAGChatbot(prompt: str):
 
 
 if __name__ == "__main__":
-    userPrompt = input("Hi! Ask me anything about Harry Potter: \n")
+    userPrompt = None
+    chunks = splitTheKnowledgeBase()
+    retriever = EmbedTheChunks(chunks)
     
-    response = None
-    while response == None:
-        print("Reading The Books......Wait Please.....")
-        response = RAGChatbot(userPrompt)
+    while userPrompt.lower() != 'exit':
+        userPrompt = input("Hi! Ask me anything about Harry Potter: \n")
+    
+        response = None
+        while response == None:
+            print("Reading The Books......Wait Please.....")
+            response = RAGChatbot(userPrompt,retriever)
         
     
-    print(response)
-    print("Mischeif Managed")
+        print(response)
+        print("****Mischeif Managed*****")
+        userPrompt = 'exit'
 
 
